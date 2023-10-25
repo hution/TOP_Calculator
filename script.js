8f08f7b583d493b2c5056a78fcb769f28ca4ab0e
Travis Hawk <hawk.tr@gmail.com>
2023-10-25 00:43:03 -0700

    Added display bar. Numbers, clear, backspace are all functional.

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const body = document.querySelector("body");
+
 const bodyContainer = document.querySelector("#container");
+bodyContainer.setAttribute('id','bodyContainer');
+
+let buttonInfo = [
+    {textContent:"CLR",operator:"CLR"},
+    {textContent:" ",operator:""},
+    {textContent:"<-",operator:"BCK"},
+    {textContent:"/",operator:"/"},
+    {textContent:"7",operator:"digit"},
+    {textContent:"8",operator:"digit"},
+    {textContent:"9",operator:"digit"},
+    {textContent:"*",operator:"*"},
+    {textContent:"4",operator:"digit"},
+    {textContent:"5",operator:"digit"},
+    {textContent:"6",operator:"digit"},
+    {textContent:"-",operator:"-"},
+    {textContent:"1",operator:"digit"},
+    {textContent:"2",operator:"digit"},
+    {textContent:"3",operator:"digit"},
+    {textContent:"+",operator:"+"},
+    {textContent:" ",operator:""},
+    {textContent:"0",operator:"digit"},
+    {textContent:" ",operator:""},
+    {textContent:"=",operator:"="},
+]
 
 createCalculator();
 
@@ -38,21 +63,114 @@ function operate(operator, val1, val2) {
 }
 
 function createCalculator(){
+    //Create the Display Bar
+    const divDisplayBar = document.createElement('div');
+    divDisplayBar.setAttribute('id','divDisplayBar');
+    bodyContainer.appendChild(divDisplayBar)
+
+
+
+
     //Create the buttons for 0-9, +, -, *,/,clear,=
 
-    for(i=0;i<4;i++){
+    for(i=0;i<5;i++){
         const divRowContainer = document.createElement('div');
         divRowContainer.setAttribute('id','divRowContainer');
         bodyContainer.appendChild(divRowContainer);
         
         for(j=0;j<4;j++){
             const div = document.createElement('button');
-            div.setAttribute('id','calcButton');
+            div.setAttribute('class','calcButton');
+            // div.setAttribute('id',buttonInfo[j+(i*4)].idVal;);
+            div.textContent=buttonInfo[j+(i*4)].textContent;
+
             divRowContainer.appendChild(div);    
         }
     }
 
 }
+function getCurrentDisplay(){
+    return document.getElementById('divDisplayBar').textContent;
+}
+
+function updateDisplay(newVal){
+    let currentValue = getCurrentDisplay();
+    divDisplayBar.textContent=currentValue+newVal;
+
+}
+
+function clearDisplay(){
+    divDisplayBar.textContent="";
+}
+
+function backSpace(){
+    let currentValue = getCurrentDisplay();
+    divDisplayBar.textContent=currentValue.slice(0,-1);
+}
+
+const buttonList = document.querySelectorAll('.calcButton');
+buttonList.forEach((button) => {
+    button.addEventListener('click', function (e) {
+        switch(e.target.textContent){
+            case "1":
+                updateDisplay(e.target.textContent);
+                break;
+            case "2":
+                updateDisplay(e.target.textContent);
+                break;
+            case "3":
+                updateDisplay(e.target.textContent);
+                break;
+            case "4":
+                updateDisplay(e.target.textContent);
+                break;
+            case "5":
+                updateDisplay(e.target.textContent);
+                break;
+            case "6":
+                updateDisplay(e.target.textContent);
+                break;
+            case "7":
+                updateDisplay(e.target.textContent);
+                break;
+            case "8":
+                updateDisplay(e.target.textContent);
+                break;
+            case "9":
+                updateDisplay(e.target.textContent);
+                break;
+            case "0":
+                updateDisplay(e.target.textContent);
+                break;
+            case "+":
+                updateDisplay(" " + e.target.textContent+ " ");
+                break;
+            case "-":
+                updateDisplay(" " + e.target.textContent+ " ");
+                break;
+            case "/":
+                updateDisplay(" " + e.target.textContent+ " ");
+                break;
+            case "*":
+                updateDisplay(" " + e.target.textContent+ " ");
+                break;
+            case "=":
+                console.log("= button");
+                break;
+            case "<-":
+                backSpace();
+                break;
+            case "CLR":
+                clearDisplay();
+                break;  
+            default:
+                console.log("reached default in click event")
+                break;
+        }
+    });
+
+});
+
 
 console.log(operate("+",10,31));
 console.log(operate("-",10,31));
